e135f25d0eb62ab1eafab235e99bd5eed03e25ad
polamjag <s@polamjag.info>
2020-10-03 17:55:26 +0900

    Revert "TeamCapacity               = 90"
    This reverts commit 00d017e2bfd72c4c0cdfe72943307e652d8ef392.

--- webapp/golang/cmd/xsuportal/main.go
+++ webapp/golang/cmd/xsuportal/main.go
@@ -36,7 +36,7 @@ import (
 )
 
 const (
-	TeamCapacity               = 90
+	TeamCapacity               = 65
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
