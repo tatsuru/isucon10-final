97cf90fad6738579710b41342c3ecc734928499d
wtatsuru <wtatsuru@hatena.ne.jp>
2020-10-10 02:00:31 +0900

    Revert "Revert "120""
    This reverts commit 709557d41ccc66b67b466c2dad96dd0c592e3a10.

--- webapp/golang/cmd/xsuportal/main.go
+++ webapp/golang/cmd/xsuportal/main.go
@@ -36,7 +36,7 @@ import (
 )
 
 const (
-	TeamCapacity               = 65
+	TeamCapacity               = 120
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
